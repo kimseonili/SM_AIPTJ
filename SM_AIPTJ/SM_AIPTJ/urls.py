d04de3b82d9f538fdda6ef030a477dfeb14d6dfc
Jinwoo Jung <wjdwlsdn1216@naver.com>
2020-09-10 17:54:51 +0900

    test

--- SM_AIPTJ/SM_AIPTJ/urls.py
+++ SM_AIPTJ/SM_AIPTJ/urls.py
@@ -19,5 +19,6 @@ from SM_AIPTJ import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('alphapose/',views.alphapose, name='alphapose'),
 
 ]
