8ab832f38bab4ace87b2032bb6a981e62cd3ebbf
Jinwoo Jung <wjdwlsdn1216@naver.com>
2020-09-15 17:24:02 +0900

    SM

---
+++ server1/SM/SM_AP/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def video(request):
+    return render(request,'video.html')
+
+def result(request):
+    return render(request,'result.html')
+
+def upload(request):
+    return render(request,'result.html')
